410707f604c19c448f78b1ad30e56e1c2654ce52
Altryd <michael_bor@mail.ru>
2025-07-20 23:04:43 +0400

    Из демо-2 перенесена структура, добавлен docker-compose для postgres и начата работа над сервером

---
+++ src/backend/database.py
@@ -0,0 +1,102 @@
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String, Text, ForeignKey, Enum, Boolean, DateTime, func, Float, JSON
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+import enum
+from src.config.config import Config
+
+Base = declarative_base()
+
+
+class Role(enum.Enum):
+    user = "user"
+    assistant = "assistant"
+    system = "system"
+
+
+class User(Base):
+    __tablename__ = "user"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(100), nullable=False)
+    chats = relationship("Chat", back_populates="user")
+    is_deleted = Column(Boolean, default=False, nullable=False)
+    speed_results = relationship("SpeedTestResult", back_populates="user")
+
+
+class Chat(Base):
+    __tablename__ = "chat"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(
+        Integer,
+        ForeignKey(
+            "user.id",
+            ondelete="CASCADE"),
+        nullable=False)
+    summary = Column(String(255), nullable=True)
+
+    created_at = Column(DateTime(timezone=True), nullable=True)
+
+    user = relationship("User", back_populates="chats")
+    messages = relationship("Message", back_populates="chat")
+    is_deleted = Column(Boolean, default=False, nullable=False)
+
+
+class Message(Base):
+    __tablename__ = "message"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    chat_id = Column(
+        Integer,
+        ForeignKey(
+            "chat.id",
+            ondelete="CASCADE"),
+        nullable=False)
+    text = Column(Text, nullable=False)
+    role = Column(Enum(Role), nullable=False)
+    chat = relationship("Chat", back_populates="messages")
+    is_deleted = Column(Boolean, default=False, nullable=False)
+
+
+engine = create_engine(
+    Config.POSTGRES_DATABASE_URL, echo=True,
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+class SpeedTestResult(Base):
+    __tablename__ = "speed_test_result"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer,
+                     ForeignKey(
+                         "user.id",
+                         ondelete="CASCADE"
+                     ),
+                     nullable=False, index=True)
+    user = relationship("User", back_populates="speed_results")
+    taps = Column(Integer, nullable=False)
+    time = Column(Float, nullable=False)
+    stream_speed = Column(Float, nullable=False) # BPM
+    unstable_rate = Column(Float, nullable=False)
+    chart_data = Column(JSON, nullable=True) # Поле для хранения данных графика
+    timestamp = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+
+
+postgres_engine = create_engine(
+    Config.POSTGRES_DATABASE_URL,
+    echo=True
+)
+
+PostgresSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=postgres_engine)
+
+
+def get_db():
+    db = PostgresSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+def create_postgres_tables():
+    Base.metadata.create_all(bind=postgres_engine)
---
+++ src/config/config.py
@@ -0,0 +1,40 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Config:
+    MYSQL_DATABASE_URL = (f"mysql+mysqlconnector://{os.getenv('MYSQL_USER')}:"
+                          f"{os.getenv('MYSQL_PASSWORD')}@127.0.0.1:3308/{os.getenv('MYSQL_DATABASE')}")
+    FILL_MYSQL_IF_EMPTY = True
+    POSTGRES_DATABASE_URL = (f"postgresql+pg8000://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@"
+                             f"{os.getenv('POSTGRES_HOST')}:{os.getenv('POSTGRES_PORT')}/{os.getenv('POSTGRES_DB')}")
+
+    IMGBB_API_KEY = os.getenv("IMGBB_API_KEY")
+
+    USE_CASE = "API"
+    LANGSMITH_TRACING = True
+    LANGSMITH_ENDPOINT = "https://api.smith.langchain.com"
+    LANGSMITH_PROJECT = "pr-notable-divider-48"
+
+    LLM_API_KEY = os.getenv("KIMI_API_KEY")
+    LLM_BASE_URL = "https://openrouter.ai/api/v1"
+    LLM_MODEL = "google/gemma-3n-e2b-it:free"
+
+    EMBEDDING_MODEL = "paraphrase-multilingual-MiniLM-L12-v2"
+    MONGODB_URI = os.getenv("MONGODB_URI", "mongodb://localhost:27017")
+    MONGODB_DB = "osu_agent"
+    MONGODB_COLLECTION = "documents"
+    REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6381/0")
+    REDIS_PASSWORD = os.getenv("REDIS_PASSWORD")
+
+    FASTAPI_HOST = "0.0.0.0"
+    FASTAPI_PORT = 8000
+
+    MAX_QUERY_LENGTH = 500
+    ALLOWED_LANGUAGES = ["ru", "en"]
+
+    DATA_DIR = "data/"
+    RAG_DIR = "rag_database"
+    FT_DIR = "fine-tuning/dataset"
